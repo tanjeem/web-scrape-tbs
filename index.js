6e65772f35cd5ee01626a0b6ac867171eb6be728
tanjeem <tanjeem.adeeb@gmail.com>
2022-01-03 23:37:41 +0600

    scrape

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT=8000
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { response } = require('express');
+const express = require('express')
+const app = express()
+
+const url= 'https://www.tbsnews.net/'
+axios(url)
+    .then(response =>{
+        const html = response.data
+        const $  = cheerio.load(html)
+        const stock = []
+
+        $('.card-title' , html).each(function(){
+            const title = $(this).text()
+            const url =$(this).find('a').attr('href')
+            stock.push({
+                title,
+                url
+            })
+            
+
+        })
+        console.log(stock)
+        
+    }).catch(err => console.log(err))
+    
+app.listen(PORT , () => console.log(`server is running on port ${PORT}`))
